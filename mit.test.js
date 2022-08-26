b4a38fc01dec1eb4331c94b4a9daad3118d68fbf
wewtw <e8tlis@gmail.com>
2022-08-26 12:01:19 -0400

    Fixed Date wojest mod, Colors, Added correct mascot.

--- mit.test.js
+++ mit.test.js
@@ -22,8 +22,8 @@ describe('MIT', () => {
     });
 
     test('Founded range', () => {
-        expect(mit.founded).toBeLessThanOrEqual(1861);
-        expect(mit.founded).toBeGreaterThanOrEqual(1861);    
+        expect(mit.founded).toBeLessThanOrEqual(1875);
+        expect(mit.founded).toBeGreaterThanOrEqual(1850);    
     });
 
     test('Motto', () => {
